9b833d29c49e3716d21b952669fb23bcf6119aea
mahesh-1998 <maheshvkamble674@gmail.com>
2020-07-03 18:34:34 +0530

    added view and setup urls

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+# Create your views here.    
+def index(request):
+    template_name = 'weather_app/base.html'
+    return render(request,template_name)
--- weather_project/settings.py
+++ weather_project/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    if '64' in platform.architecture()[0]:
+        OSGEO4W += "64"
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'leaflet',
+    'weather_app',
 ]
 
 MIDDLEWARE = [
@@ -78,9 +89,9 @@ WSGI_APPLICATION = 'weather_project.wsgi.application'
 DATABASES = {
     'default': {
          'ENGINE': 'django.contrib.gis.db.backends.postgis',
-         'NAME': 'geodjango',
+         'NAME': 'world_new',
          'USER': 'root',
-         'PASSWORD': 'ROOT',
+         'PASSWORD': 'root',
          'PORT' : '5432',
     },
 }
