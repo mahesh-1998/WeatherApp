ccd45ee2d54a1267d854c6362f234aec086450b6
mahesh-1998 <maheshvkamble674@gmail.com>
2020-07-05 11:15:10 +0530

    Index view defined

--- weather_app/views.py
+++ weather_app/views.py
@@ -3,8 +3,18 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from weather_app.models import City
 
 # Create your views here.    
 def index(request):
+    context = {}
     template_name = 'weather_app/base.html'
-    return render(request,template_name)
+    
+    if request.method == 'POST':
+        city = request.POST.get('city')
+        #print(city)
+        context['city'] = City.objects.get(name=city)
+        #print(context['city'])
+        return render(request,template_name,context)
+    else:
+        return render(request,template_name,context)
