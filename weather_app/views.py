95de80d85090e922fd29a96094e83816d19be848
mahesh-1998 <maheshvkamble674@gmail.com>
2020-07-05 17:17:18 +0530

    Added weather api error handling remaining

--- weather_app/views.py
+++ weather_app/views.py
@@ -4,17 +4,23 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from weather_app.models import City
+import requests
+import xml.etree.ElementTree as ET
+import urllib.parse,urllib.request,urllib.error
+import json
 
 # Create your views here.    
 def index(request):
     context = {}
     template_name = 'weather_app/base.html'
+    get_points_url = "https://api.weather.gov/points/"
     
     if request.method == 'POST':
-        city = request.POST.get('city')
-        #print(city)
-        context['city'] = City.objects.get(name=city)
-        #print(context['city'])
+        cord = request.POST.get('cord')
+        pointsRes = requests.get(get_points_url+cord).json() 
+        hourlyRes = requests.get(pointsRes['properties']["forecastHourly"]).json()
+        temp = hourlyRes["properties"]['periods'][0]['temperature']
+        context['temp'] = temp
         return render(request,template_name,context)
     else:
         return render(request,template_name,context)
