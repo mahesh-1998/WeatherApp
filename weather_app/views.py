d57ee0d387b4f36520451767506b05626929162e
mahesh-1998 <maheshvkamble674@gmail.com>
2020-07-05 22:13:57 +0530

    Search by city added

--- weather_app/views.py
+++ weather_app/views.py
@@ -13,14 +13,43 @@ import json
 def index(request):
     context = {}
     template_name = 'weather_app/base.html'
+    template_error = 'weather_app/error.html'
     get_points_url = "https://api.weather.gov/points/"
     
     if request.method == 'POST':
-        cord = request.POST.get('cord')
-        pointsRes = requests.get(get_points_url+cord).json() 
-        hourlyRes = requests.get(pointsRes['properties']["forecastHourly"]).json()
-        temp = hourlyRes["properties"]['periods'][0]['temperature']
-        context['temp'] = temp
-        return render(request,template_name,context)
+        if request.POST.get('cord'):
+            try:
+                cord = request.POST.get('cord')
+                pointsRes = requests.get(get_points_url+cord).json() 
+                hourlyRes = requests.get(pointsRes['properties']["forecastHourly"]).json()
+                temp = hourlyRes["properties"]['periods'][0]['temperature']
+                img =  hourlyRes["properties"]['periods'][0]['icon']
+                shortDesc = hourlyRes["properties"]['periods'][0]['shortForecast']
+                context['temp'] = temp
+                context['img'] = img
+                context['desc'] = shortDesc
+            except:
+                return render(request,template_error,context)
+            return render(request,template_name,context)
+        else:
+            try:
+                city = request.POST.get('city')
+                city_obj = City.objects.get(name=city)
+                lat = city_obj.latitude
+                lng = city_obj.longitude 
+                print(city_obj)
+                cord = str(lat) +","+ str(lng)
+                print(cord)
+                pointsRes = requests.get(get_points_url+cord).json() 
+                hourlyRes = requests.get(pointsRes['properties']["forecastHourly"]).json()
+                temp = hourlyRes["properties"]['periods'][0]['temperature']
+                img =  hourlyRes["properties"]['periods'][0]['icon']
+                shortDesc = hourlyRes["properties"]['periods'][0]['shortForecast']
+                context['temp'] = temp
+                context['img'] = img
+                context['desc'] = shortDesc
+            except:
+                return render(request,template_error,context)
+            return render(request,template_name,context)
     else:
         return render(request,template_name,context)
