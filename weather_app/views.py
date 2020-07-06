51ce728f8b297895aaca51aa8a3d361da303ae91
mahesh-1998 <maheshvkamble674@gmail.com>
2020-07-06 07:12:44 +0530

    Documented code

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,55 +1,58 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
+import urllib.parse,urllib.request,urllib.error
+import xml.etree.ElementTree as ET
+#import City model
 from weather_app.models import City
 import requests
-import xml.etree.ElementTree as ET
-import urllib.parse,urllib.request,urllib.error
 import json
 
-# Create your views here.    
+# View for 'weather/'
 def index(request):
+    # templates, context, API Url
     context = {}
-    template_name = 'weather_app/base.html'
+    template_base = 'weather_app/base.html'
     template_error = 'weather_app/error.html'
     get_points_url = "https://api.weather.gov/points/"
     
     if request.method == 'POST':
-        if request.POST.get('cord'):
+        
+        if request.POST.get('cord'):        # if search by co-ordinates is selected.
             try:
                 cord = request.POST.get('cord')
                 pointsRes = requests.get(get_points_url+cord).json() 
-                hourlyRes = requests.get(pointsRes['properties']["forecastHourly"]).json()
+                hourlyRes = requests.get(pointsRes['properties']["forecastHourly"]).json()      # Scraping weather details from API 
                 temp = hourlyRes["properties"]['periods'][0]['temperature']
                 img =  hourlyRes["properties"]['periods'][0]['icon']
                 shortDesc = hourlyRes["properties"]['periods'][0]['shortForecast']
-                context['temp'] = temp
-                context['img'] = img
-                context['desc'] = shortDesc
-            except:
+                context = {
+                    'temp': temp,
+                    'img' : img,
+                    'desc' : shortDesc
+                }   
+            except: 
                 return render(request,template_error,context)
-            return render(request,template_name,context)
-        else:
+            
+            return render(request,template_base,context)
+        
+        else:                                  # if search by city is seleceted. 
             try:
                 city = request.POST.get('city')
                 city_obj = City.objects.get(name=city)
-                lat = city_obj.latitude
-                lng = city_obj.longitude 
-                print(city_obj)
-                cord = str(lat) +","+ str(lng)
-                print(cord)
                 pointsRes = requests.get(get_points_url+cord).json() 
                 hourlyRes = requests.get(pointsRes['properties']["forecastHourly"]).json()
                 temp = hourlyRes["properties"]['periods'][0]['temperature']
                 img =  hourlyRes["properties"]['periods'][0]['icon']
-                shortDesc = hourlyRes["properties"]['periods'][0]['shortForecast']
-                context['temp'] = temp
-                context['img'] = img
-                context['desc'] = shortDesc
+                shortDesc = hourlyRes["properties"]['periods'][0]['shortForecast']      # Scraping weather details from API 
+                context = {
+                    'temp' : temp,
+                    'img' : img,
+                    'desc' : shortDesc
+                }
             except:
-                return render(request,template_error,context)
-            return render(request,template_name,context)
+                 return render(request,template_error,context)
+            
+            return render(request,template_base,context)
+        
     else:
-        return render(request,template_name,context)
+
+        return render(request,template_base,context) # If GET method hits.
